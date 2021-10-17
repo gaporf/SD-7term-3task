5a6c7eb47201041592eb375d03d8c0a035ae6d4a
Nikolai Akimov <gaporf@mail.ru>
2021-10-18 00:43:27 +0300

    remove copy paste from QueryServlet.java

--- src/main/java/ru/ifmo/rain/akimov/servlet/QueryServlet.java
+++ src/main/java/ru/ifmo/rain/akimov/servlet/QueryServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class QueryServlet extends HttpServlet {
     final SQLDataBase dataBase;
@@ -22,31 +23,36 @@ public class QueryServlet extends HttpServlet {
         String command = request.getParameter("command");
 
         if ("max".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with max price: </h1>");
-            final Optional<DataBaseProduct> product = dataBase.getProductWithMaximalPrice();
-            if (product.isPresent()) {
-                response.getWriter().println(product.get().getName() + "\t" + product.get().getPrice() + "</br>");
-            }
-            response.getWriter().println("</body></html>");
+            runCommand(
+                    response,
+                    () -> {
+                        final StringBuilder stringBuilder = new StringBuilder();
+                        final Optional<DataBaseProduct> product = dataBase.getProductWithMaximalPrice();
+                        product.ifPresent(p -> stringBuilder.append(p.getName()).append("\t").append(p.getPrice()).append("</br>"));
+                        if (!stringBuilder.isEmpty()) {
+                            stringBuilder.append(System.lineSeparator());
+                        }
+                        return stringBuilder.toString();
+                    },
+                    "<h1>Product with max price: </h1>"
+            );
         } else if ("min".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with min price: </h1>");
-            final Optional<DataBaseProduct> product = dataBase.getProductWithMinimalPrice();
-            if (product.isPresent()) {
-                response.getWriter().println(product.get().getName() + "\t" + product.get().getPrice() + "</br>");
-            }
-            response.getWriter().println("</body></html>");
+            runCommand(
+                    response,
+                    () -> {
+                        final StringBuilder stringBuilder = new StringBuilder();
+                        final Optional<DataBaseProduct> product = dataBase.getProductWithMinimalPrice();
+                        product.ifPresent(p -> stringBuilder.append(p.getName()).append("\t").append(p.getPrice()).append("</br>"));
+                        if (!stringBuilder.isEmpty()) {
+                            stringBuilder.append(System.lineSeparator());
+                        }
+                        return stringBuilder.toString();
+                    },
+                    "<h1>Product with min price: </h1>");
         } else if ("sum".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Summary price: ");
-            response.getWriter().println(dataBase.getSum());
-            response.getWriter().println("</body></html>");
+            runCommand(response, () -> dataBase.getSum() + System.lineSeparator(), "Summary price: ");
         } else if ("count".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Number of products: ");
-            response.getWriter().println(dataBase.getCount());
-            response.getWriter().println("</body></html>");
+            runCommand(response, () -> dataBase.getCount() + System.lineSeparator(), "Number of products: ");
         } else {
             response.getWriter().println("Unknown command: " + command);
         }
@@ -55,4 +61,10 @@ public class QueryServlet extends HttpServlet {
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
+    private void runCommand(final HttpServletResponse response, final Supplier<String> supplier, final String comment) throws IOException {
+        response.getWriter().println("<html><body>");
+        response.getWriter().println(comment);
+        response.getWriter().print(supplier.get());
+        response.getWriter().println("</body></html>");
+    }
 }
